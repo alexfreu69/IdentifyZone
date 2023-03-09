aef9768a65a69d10e17edc6aa6e4c019e92b9d8f
a.freudenberg <a.freudenberg@DEPNB80292>
2023-03-09 11:25:37 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace IdentifyZone2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(398, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Check Zone";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(36, 35);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(19, 20);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "_";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 67);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "IdentifyZone";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace IdentifyZone2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Zone: " + GetZoneName(CheckSecurityZone(textBox1.Text));
+        }
+
+        private string GetZoneName(int zone)
+        {
+            switch (zone)
+            {
+                case 4:
+                    return "Untrusted";
+                case 3:
+                    return "Internet";
+                case 2:
+                    return "Trusted";
+                case 1:
+                    return "Local Intranet";
+                case 0:
+                    return "My Computer";
+                case -1:
+                    return "Function error!";
+                case -2:
+                    return "COMException";
+                case -3:
+                    return "Unknown Zone!";
+                default:
+                    return "";
+	        }
+        }
+
+
+
+        private int CheckSecurityZone(string url)
+        {
+            int _ret = -4;
+
+            try
+            {
+                IInternetSecurityManager _ism = IISMFactory.GetISM();
+                UInt32 _pdwZone = 0;
+                int _res = _ism.MapUrlToZone(url, out _pdwZone, 0);
+                if (_res == 0)
+                {
+                    if (_pdwZone < 5)
+                        _ret = (int)_pdwZone;
+                    else
+                        _ret = -3;
+                }
+                else
+                    _ret = -1;
+            }
+            catch (COMException)
+            {
+                _ret = -2;
+            }
+
+            return _ret;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] x = Environment.GetCommandLineArgs();
+            if (x.Length == 3)
+            {
+                string res = GetZoneName(CheckSecurityZone(x[1]));
+                System.IO.File.WriteAllText(x[2], x[1]+ " => " + res); 
+                Application.Exit();
+            }
+        }
+    }
+
+    #region IInternetSecurityManager Interface
+    [ComVisible(true), ComImport,
+    GuidAttribute("79EAC9EE-BAF9-11CE-8C82-00AA004BA90B"),
+    InterfaceTypeAttribute(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IInternetSecurityManager
+    {
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int SetSecuritySite(
+            [In] IntPtr pSite);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetSecuritySite(
+            out IntPtr pSite);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int MapUrlToZone(
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+            out UInt32 pdwZone,
+            UInt32 dwFlags);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetSecurityId(
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+            [Out] IntPtr pbSecurityId, [In, Out] ref UInt32 pcbSecurityId,
+            [In] ref UInt32 dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int ProcessUrlAction(
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+            UInt32 dwAction,
+            IntPtr pPolicy, UInt32 cbPolicy,
+            IntPtr pContext, UInt32 cbContext,
+            UInt32 dwFlags,
+            UInt32 dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int QueryCustomPolicy(
+            [In, MarshalAs(UnmanagedType.LPWStr)] string pwszUrl,
+            ref Guid guidKey,
+            out IntPtr ppPolicy, out UInt32 pcbPolicy,
+            IntPtr pContext, UInt32 cbContext,
+            UInt32 dwReserved);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int SetZoneMapping(
+            UInt32 dwZone,
+            [In, MarshalAs(UnmanagedType.LPWStr)] string lpszPattern,
+            UInt32 dwFlags);
+
+        [return: MarshalAs(UnmanagedType.I4)]
+        [PreserveSig]
+        int GetZoneMappings(
+            [In] UInt32 dwZone, //One or more of tagURLZONE enums
+            out IEnumString ppenumString,
+            [In] UInt32 dwFlags);
+    }
+    public static class IISMFactory
+    {
+        public static Guid CLSID_InternetSecurityManager = new Guid("7b8a2d94-0ac9-11d1-896c-00c04fb6bfc4");
+        public static Guid IID_IInternetSecurityManager = new Guid("79eac9ee-baf9-11ce-8c82-00aa004ba90b");
+
+        public static IInternetSecurityManager GetISM()
+        {
+            Type t = Type.GetTypeFromCLSID(CLSID_InternetSecurityManager);
+            object securityManager = Activator.CreateInstance(t);
+            IInternetSecurityManager ism = (IInternetSecurityManager)securityManager;
+            return ism;
+        }
+    }
+
+    #endregion
+
+}
